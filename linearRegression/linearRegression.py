71d4cdc742b20f16d6d7d1a01a08597dc68004a5
Guilherme Tezoli Bakaukas <61765419+guilherme-bakaukas@users.noreply.github.com>
2021-05-07 12:32:29 -0300

    Update linearRegression.py
    Adicionando gráfico para ilustrar

--- linearRegression/linearRegression.py
+++ linearRegression/linearRegression.py
@@ -1,15 +1,24 @@
 from statistics import mean
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Y = mX + b
-#aqui temos um exemplo de cálculo do coeficiente angular 'm' utilizando a regressão linear
+# Aqui temos um exemplo de cálculo do coeficiente angular 'm' e coeficiente linear 'b' utilizando a regressão linear
 
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7],dtype=np.float64)
 
-def define_m(xs,ys):
-    m=(mean(xs)*mean(ys)-mean(xs*ys))/((mean(xs)*mean(xs))-mean(xs*xs))
-    return m
+def define_coeficients(xs,ys):
+    m = (mean(xs)*mean(ys)-mean(xs*ys))/((mean(xs)*mean(xs))-mean(xs*xs))
+    b = mean(ys) - m*mean(xs)
+    return m, b
 
-m = define_m(xs,ys)
+m, b = define_coeficients(xs,ys)
 print("Coeficiente angular: ", m)
+print("Coeficiente linear: ", b)
+
+regression_Y = m*xs + b
+
+plt.plot(xs, regression_Y, color="blue")
+plt.scatter(xs, ys, color='red')
+plt.show()
